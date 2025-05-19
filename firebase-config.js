0594a39e3991476a639937c3e87d7ef854513970
PRECIOUS AHUEKWE <etherxchange6@gmail.com>
2025-05-19 02:46:00 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgOvIkDCcS3DFrt5f6Y7pYmjCoHIfDF9U",
+  authDomain: "moniebase-b4b4d.firebaseapp.com",
+  projectId: "moniebase-b4b4d",
+  storageBucket: "moniebase-b4b4d.appspot.com",
+  messagingSenderId: "1009678808253",
+  appId: "1:1009678808253:android:ff94e94c52bad2db7973d4",
+  databaseURL: "https://moniebase-b4b4d-default-rtdb.firebaseio.com/",
+};
+
+// In a production environment, you should use environment variables
+// to store these sensitive values
